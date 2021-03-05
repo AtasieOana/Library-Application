6c7bfde13ae88d50fa0bfb651e982a5795da0111
Atasie Oana <oana.atasie@s.unibuc.ro>
2021-03-05 18:12:00 +0200

    library-application

---
+++ src/Classes/Librarian.java
@@ -0,0 +1,53 @@
+package Classes;
+
+import java.util.Date;
+
+public class Librarian {
+
+    private String lastName;
+    private String firstName;
+    private Date dateOfBirth;
+    private String address;
+    private int salary;
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
