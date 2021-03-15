b23b214f468272369d7c9623152197fe11426bb4
Atasie Oana <oana.atasie@s.unibuc.ro>
2021-03-15 15:56:40 +0200

    library-application

--- src/Services/Service.java
+++ src/Services/Service.java
@@ -9,6 +9,7 @@ public class Service {
 
     private Library library;
 
+    /** Adding a book in the library **/
     public void addBookInLibrary(String name, int numberOfPages, int yearOfPublication, String language,
                                  LibraryAuthor libraryAuthor, Section section, int numberOfCopies){
         if(!library.findSection(section)){
