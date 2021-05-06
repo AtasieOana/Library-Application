73525b4f7311538d321d0d3450bd9eb945714dce
Atasie Oana <oana.atasie@s.unibuc.ro>
2021-05-06 20:23:13 +0300

    second-part-of-the-project

--- src/Services/HelperService.java
+++ src/Services/HelperService.java
@@ -6,7 +6,7 @@ import Classes.SectionType;
 import java.util.Calendar;
 import java.util.Date;
 
-public class HelperService<T> {
+public class HelperService {
     /**
      * Creating a date with the given year, month and day
      */
@@ -28,24 +28,12 @@ public class HelperService<T> {
     public static Section createSectionWithSectionType(String type){
         Section section = new Section();
         switch (type.toUpperCase()) {
-            case "FICTIONAL" -> {
-                section = new Section(SectionType.FICTIONAL);
-            }
-            case "REFERENCE" -> {
-                section = new Section(SectionType.REFERENCE);
-            }
-            case "NEWS" -> {
-                section = new Section(SectionType.NEWS);
-            }
-            case "CHILDREN" -> {
-                section = new Section(SectionType.CHILDREN);
-            }
-            case "NONFICTION" -> {
-                section = new Section(SectionType.NONFICTION);
-            }
-            case "POEMS" -> {
-                section = new Section(SectionType.POEMS);
-            }
+            case "FICTIONAL" -> section = new Section(SectionType.FICTIONAL);
+            case "REFERENCE" -> section = new Section(SectionType.REFERENCE);
+            case "NEWS" -> section = new Section(SectionType.NEWS);
+            case "CHILDREN" -> section = new Section(SectionType.CHILDREN);
+            case "NONFICTION" -> section = new Section(SectionType.NONFICTION);
+            case "POEMS" -> section = new Section(SectionType.POEMS);
             default -> {
                 System.out.println("Section Type is wrong!");
                 System.exit(0);
